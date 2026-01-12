fec308d9d0a85f6a5fce0c3942617b45259556aa
satish dex <satish.dexbytes@gmail.com>
2026-01-12 21:42:20 +0530

    add sahaj optical

--- pages/layout.js
+++ pages/layout.js
@@ -1,17 +1,7 @@
 
 export default function RootLayout({ children }) {
-   const path = headers().get("x-pathname") || "";
-
-  const manifest =
-    path.startsWith("/admin")
-      ? "/manifest-admin.json"
-      : "/manifest.json";
-
   return (
     <html lang="en">
-       <head>
-        <link rel="manifest" href={manifest} />
-      </head>
       <body>{children}</body>
     </html>
   )
